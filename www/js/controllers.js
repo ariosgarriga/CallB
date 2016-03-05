23d9bf53020e1a83dc575bb3022af56638981be1
ariosgarriga <ariosgarriga@gmail.com>
2016-03-05 14:52:33 -0430

    4

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,8 +2,17 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('SolicitudCtrl', function($scope, Chats) {
+.controller('SolicitudCtrl', function($scope, $http) {
+  $http.get('http://mysafeinfo.com/api/data?list=teamlist_us&format=json').then(function(resp) {
+    console.log('Success', resp);
+    // For JSON responses, resp.data contains the result
+    $scope.equipos=resp.data;
 
+  }, function(err) {
+    console.error('ERR', err);
+    // err.status will contain the status code
+  })
+  
 })
 
 
